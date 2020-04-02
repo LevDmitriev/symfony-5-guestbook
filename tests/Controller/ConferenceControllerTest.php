8b4b45e1a76344e71616027a2e29055ea6a70dc5
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2020-04-02 21:40:22 +0400

    17.10 Очистка базы данных после каждого теста

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -15,19 +15,7 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertSelectorTextContains('h2', 'Give your feedback');
     }
 
-    public function testConferencePage()
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-        $this->assertCount(2, $crawler->filter('h4'));
 
-        $client->click($crawler->filter('h4 + p a')->link());
-
-        $this->assertPageTitleContains('Amsterdam');
-        $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
-        $this->assertSelectorExists('div:contains("There are 1 comments")');
-    }
 
     public function testCommentSubmission()
     {
@@ -42,5 +30,19 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseRedirects();
         $client->followRedirect();
         $this->assertSelectorExists('div:contains("There are 2 comments")');
+    }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->click($crawler->filter('h4 + p a')->link());
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
