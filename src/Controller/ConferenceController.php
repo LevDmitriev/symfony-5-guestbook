4a9b9d19ad27b93323d2187382d80c505c1cbe8e
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2020-04-12 22:18:50 +0400

    25.1 Отправка уведомлений в браузере

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -15,6 +15,8 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -65,6 +67,7 @@ class ConferenceController extends AbstractController
         Conference $conference,
         CommentRepository $commentRepository,
         ConferenceRepository $conferenceRepository,
+        NotifierInterface $notifier,
         string $photoDir
     )
     {
@@ -94,7 +97,19 @@ class ConferenceController extends AbstractController
             ];
             $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
+            $notifier->send(
+                new Notification(
+                    'Thank you for the feedback; your comment will be posted after moderation',
+                    ['browser']
+                )
+            );
+
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+        if ($form->isSubmitted()) {
+            $notifier->send(
+                new Notification('Can you check your submission? There are some problems with it.', ['browser'])
+            );
         }
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
