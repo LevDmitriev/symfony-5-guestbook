756a31548d405d3a41c56ad1117d7afa449c564b
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2020-04-12 22:54:18 +0400

    25.3 Отправка уведомлений в чаты для администраторов

--- src/Notification/CommentReviewNotification.php
+++ src/Notification/CommentReviewNotification.php
@@ -3,20 +3,28 @@
 namespace App\Notification;
 
 use App\Entity\Comment;
+use Symfony\Component\Notifier\Bridge\Slack\Block\SlackActionsBlock;
+use Symfony\Component\Notifier\Bridge\Slack\Block\SlackDividerBlock;
+use Symfony\Component\Notifier\Bridge\Slack\Block\SlackSectionBlock;
+use Symfony\Component\Notifier\Bridge\Slack\SlackOptions;
+use Symfony\Component\Notifier\Message\ChatMessage;
 use Symfony\Component\Notifier\Message\EmailMessage;
+use Symfony\Component\Notifier\Notification\ChatNotificationInterface;
 use Symfony\Component\Notifier\Notification\EmailNotificationInterface;
 use Symfony\Component\Notifier\Notification\Notification;
 use Symfony\Component\Notifier\Recipient\Recipient;
 
-class CommentReviewNotification extends Notification implements EmailNotificationInterface
+class CommentReviewNotification extends Notification implements EmailNotificationInterface, ChatNotificationInterface
 {
     private $comment;
+    private $reviewUrl;
 
-    public function __construct(Comment $comment)
+    public function __construct(Comment $comment, string $reviewUrl)
     {
         $this->comment = $comment;
 
         parent::__construct('New comment posted');
+        $this->reviewUrl = $reviewUrl;
     }
 
     public function asEmailMessage(Recipient $recipient, string $transport = null): ?EmailMessage
@@ -28,5 +36,48 @@ class CommentReviewNotification extends Notification implements EmailNotificatio
             ;
 
         return $message;
+    }
+
+    public function asChatMessage(Recipient $recipient, string $transport = null): ?ChatMessage
+    {
+        if ('slack' !== $transport) {
+            return null;
+        }
+
+        $message = ChatMessage::fromNotification($this, $recipient, $transport);
+        $message->subject($this->getSubject());
+        $message->options(
+            (new SlackOptions())
+                ->iconEmoji('tada')
+                ->iconUrl('https://guestbook.example.com')
+                ->username('Guestbook')
+                ->block((new SlackSectionBlock())->text($this->getSubject()))
+                ->block(new SlackDividerBlock())
+                ->block(
+                    (new SlackSectionBlock())
+                        ->text(
+                            sprintf(
+                                '%s (%s) says: $s',
+                                $this->comment->getAuthor(),
+                                $this->comment->getEmail(),
+                                $this->comment->getText()
+                            )
+                        )
+                )
+            ->block((new SlackActionsBlock())
+                ->button('Accept', $this->reviewUrl, 'primary')
+                ->button('Reject', $this->reviewUrl . '?reject=1', 'danger')
+            )
+        );
+    }
+
+    public function getChannels(Recipient $recipient): array
+    {
+        if (preg_match('{\b(great|awesome)\b}i', $this->comment->getText())) {
+            return ['email', 'chat/slack'];
+        }
+        $this->importance(Notification::IMPORTANCE_LOW);
+
+        return ['email'];
     }
 }
